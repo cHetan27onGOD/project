8211caa4e0c8dba1d9bc91c22c542019a0ec910c
Chetan Dinesh Banjan <145384372+cHetan27onGOD@users.noreply.github.com>
2024-06-06 22:07:27 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@
 public class hello{
     public static void main(String[] args)
     {
-        System.out.println("My name is chetan dinesh banjan\n and you will remember me as the Greatest of All Time")
+        System.out.println("My name is chetan dinesh banjan\n")
     }
 }
